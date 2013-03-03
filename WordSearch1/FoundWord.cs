8e50d3e74bd395032d913f9d9466b44507b20fe6
Jason Knight <jugisahunk@gmail.com>
2013-03-03 17:22:05 -0600

    Started work on the rough draft

---
+++ WordSearch1/FoundWord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearch1
+{
+    class FoundWord
+    {
+        string Word { get; set; }
+        Point A { get; set; }
+        Point B { get; set; }
+    }
+}
--- WordSearch1/Program.cs
+++ WordSearch1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace WordSearch1
 {
@@ -10,6 +12,43 @@ namespace WordSearch1
     {
         static void Main(string[] args)
         {
+            StreamWriter outfile = new StreamWriter(args[1]);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            //Run puzzle code
+            DoIt(new StreamReader(args[0]));
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("Time elapsed in Milliseconds: {0}", ts.Milliseconds);
+            outfile.Write(elapsedTime);
+            outfile.Flush();
+            Console.WriteLine(String.Format("RunTime: {0}", elapsedTime));
+
+            Console.ReadLine();
+        }
+
+        static void DoIt(StreamReader reader){
+            //Separate input file into rows
+                //read each row into an array
+                    //remove the line break characters
+            string[][] results;
+
+            PrepareInputData(reader, out results);
+        }
+
+        static void PrepareInputData(StreamReader reader, out string[][] results)
+        {
+            List<string> listOfStrings = new List<string>();
+
+            do
+            {         
+                listOfStrings.Add(reader.ReadLine());            
+            }
+            while (!reader.EndOfStream);
+
+            results = null;
         }
     }
 }
